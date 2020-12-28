529af63aa9172b9db78e9bf7b585cbfaec2b0eba
Dren-9843 <76239659+Dren-9843@users.noreply.github.com>
2020-12-28 07:54:37 -0800

    Add files via upload

---
+++ commands/info/rewards.js
@@ -0,0 +1,15 @@
+// const { MessageEmbed, Message } = require("discord.js")
+
+// module.exports = {
+//     name: 'reward',
+//     aliases: ['server', 'cool'],
+//     run : async(client, message) => {
+//         const embed = new MessageEmbed()
+//         .setTitle(`Hellux Kingdom!`)
+//         .setURL(`https://dsc.gg/hellux`)
+//         .setDescription(`Please Join for awesome giveaways and rewards!`)
+//         .setColor("RANDOM")
+
+//         message.channel.send(embed)
+//     }
+// }
